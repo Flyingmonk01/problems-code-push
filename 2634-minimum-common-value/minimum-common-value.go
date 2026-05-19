0d52d00ed3b98c03e2fd406c8cb7bb187c91b218
Sameer rai <sameerrai350@gmail.com>
2026-05-19 10:12:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (8.33%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.go
@@ -0,0 +1,15 @@
+func getCommon(nums1 []int, nums2 []int) int {
+    i, j := 0, 0
+    m, n := len(nums1), len(nums2)
+
+    for i < m && j < n {
+        if nums1[i] == nums2[j] {
+            return nums1[i]
+        }else if nums1[i] > nums2[j] {
+            j++
+        }else {
+            i++
+        }
+    }
+    return -1
+}
